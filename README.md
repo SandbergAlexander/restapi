497d17e312c76f7ff8912e1760c81880912bce1c
Alexander Sandberg <Alexandersandbergnystart@gmail.com>
2024-12-10 13:13:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# todo 
+# skapa rest api för api testing av Alexander Sandberg
+# skapa get endpont 
+# skapa post endpont 
+# skapa delete endpont 
+
---
+++ main.py
@@ -0,0 +1,30 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+ordlista = ["hej","hejdå"]
+testa = ordlista[0]
+print (testa)
+
+#------------------------------------------------------------
+#get
+@app.get("/user")
+def read_root():
+    return {"hej"}
+#------------------------------------------------------------
+#post
+@app.post("/user")
+def post_root():
+    return {ordlista.append("hej")}
+#------------------------------------------------------------
+#delete 
+@app.delete("/user")
+def delete_root():
+    return {"hej"}
+#------------------------------------------------------------
+#put
+@app.put("/user")
+def put_root():
+    return {"hej"}
+#------------------------------------------------------------
