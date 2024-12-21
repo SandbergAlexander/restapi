ce00dfe69eb52645b7cda2976b9c3a9aa687ee9f
Alexander Sandberg <Alexandersandbergnystart@gmail.com>
2024-12-21 15:26:49 +0100

    bug fix

--- main.py
+++ main.py
@@ -6,34 +6,32 @@ items = ["alexander", "Alexander", "sato","benny"]
 
 #------------------------------------------------------------
 #get
-@app.get("/") 
+@app.get("/",status_code=200) 
 def get_all_items(): 
     return items
 #------------------------------------------------------------
 #get
-@app.get("/user/{item_id}") 
-def get_item(item_id: str): 
-    if not item_id: 
-        raise HTTPException(status_code=400, detail="Item ID is required") 
-        items(item_id) 
+@app.get("/user/{item_id}",status_code=200) 
+def get_item(item_id: str):  
+        items.index(item_id) 
         return {"user":item_id} 
 
-    
+     
 #------------------------------------------------------------
 #post
-@app.post("/user/{item_id}") 
+@app.post("/user/{item_id}",status_code=201) 
 def create_item(item_id: str): 
         items.append(item_id) 
         return {item_id} 
 #delete
 #------------------------------------------------------------
-@app.delete("/user/{item_id}") 
+@app.delete("/user/{item_id}",status_code=204) 
 def delete_item(item_id: str): 
         items.remove(item_id) 
-        return {"Delete":item_id}
+        return {}
 #------------------------------------------------------------
 #put
-@app.put("/user/{item_id}") 
+@app.put("/user/{item_id}",status_code=201) 
 def change_item(item_id: str): 
     items.append(item_id) 
     return {"item_id": item_id}
